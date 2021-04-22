5d2038763cac00b6802175479652807d1f88343a
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-04-22 17:22:33 +0700

    fix main screen layout

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -36,67 +36,53 @@ class TourismPlaceGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isMediumSize = gridCount == 4;
-
-    var width = MediaQuery.of(context).size.width;
-    var columnWidth = width / gridCount;
-    var height = 200;
-    var aspectRatio = columnWidth / height;
     return Scrollbar(
       isAlwaysShown: true,
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: SizedBox(
-            width: isMediumSize ? 1000 : 1800,
-            child: GridView.count(
-              crossAxisCount: gridCount,
-              childAspectRatio: aspectRatio,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              children: tourismPlaceList.map((place) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return DetailScreen(place: place);
-                    }));
-                  },
-                  child: Card(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Expanded(
-                          child: Image.asset(
-                            place.imageAsset,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Text(
-                            place.name,
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(left: 8.0, bottom: 8.0),
-                          child: Text(
-                            place.location,
-                          ),
-                        ),
-                      ],
+        padding: const EdgeInsets.all(24.0),
+        child: GridView.count(
+          crossAxisCount: gridCount,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          children: tourismPlaceList.map((place) {
+            return InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailScreen(place: place);
+                }));
+              },
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        place.imageAsset,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        place.name,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                      child: Text(
+                        place.location,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
